1a1ec3ed21ae8d1fe98f49ae106d512590b5f344
Jay <1030341806@qq.com>
2024-06-13 00:55:07 +0800

    modified

--- js/mouseMove.js
+++ js/mouseMove.js
@@ -1,35 +1,18 @@
 (function () {
-    const colors = ["#D61C59", "#E7D84B", "#1B8798", "#FF5733", "#5F4B8B", "#FFC300", "#2E8B57", "#FF1493", "#4169E1", "#FF6347"];
-    const characters = ["♬", "♪", "♫", "☀", "☁", "❄", "☂", "☃", "♠", "♥", "♦", "♣", "★", "☆", "⚡", "☺", "☹", "☮", "☯", "⚘", "☘", "⚓", "✈", "❤", "❥", "❣", "☄", "✌", "☮", "☠", "☢", "☣", "✪", "✿", "❀", "❁", "❂", "❃", "❄", "❅", "❆", "❇", "❈", "❉", "❊", "❋", "❍", "❏", "❐", "❑", "❒", "❖", "❘", "❙", "❚", "❛", "❜", "❝", "❞", "❡", "❢", "❣", "❤", "❥", "❦", "❧"];
+    const COLORS = new Set(["#D61C59", "#E7D84B", "#1B8798", "#FF5733", "#5F4B8B", "#FFC300", "#2E8B57", "#FF1493", "#4169E1", "#FF6347"]);
+    const CHARACTERS = new Set(["♬", "♪", "♫", "☀", "☁", "❄", "☂", "☃", "♠", "♥", "♦", "♣", "★", "☆", "⚡", "☺", "☹", "☮", "☯", "⚘", "☘", "⚓", "✈", "❤", "❥", "❣", "☄", "✌", "☮", "☠", "☢", "☣", "✪", "✿", "❀", "❁", "❂", "❃", "❄", "❅", "❆", "❇", "❈", "❉", "❊", "❋", "❍", "❏", "❐", "❑", "❒", "❖", "❘", "❙", "❚", "❛", "❜", "❝", "❞", "❡", "❢", "❣", "❤", "❥", "❦", "❧"]);
 
-
-    const elementGroup = [];
-
+    const ELEMENT_GROUP = [];
+    
     class Element {
-        constructor() {
-            const num = Math.floor(Math.random() * characters.length);
-            this.character = characters[num];
+        constructor(x, y, color) {
+            this.character = Array.from(CHARACTERS)[Math.floor(Math.random() * CHARACTERS.size)];
             this.lifeSpan = 120;
-            this.initialStyles = {
-                position: "fixed",
-                top: "0",
-                display: "block",
-                pointerEvents: "none",
-                "z-index": "10000000",
-                fontSize: "25px",
-                "will-change": "transform",
-                color: "#000000"
-            };
-        }
-
-        init(x, y, color) {
             this.velocity = { x: (Math.random() < .5 ? -1 : 1) * (Math.random() / 2), y: 1 };
             this.position = { x: x - 10, y: y - 20 };
-            this.initialStyles.color = color;
             this.element = document.createElement("span");
             this.element.innerHTML = this.character;
-            this.applyStyles(this.initialStyles);
-            this.update();
+            this.applyStyles(color);
             document.body.appendChild(this.element);
         }
 
@@ -44,10 +27,15 @@
             this.element.parentNode.removeChild(this.element);
         }
 
-        applyStyles(style) {
-            for (let prop in style) {
-                this.element.style[prop] = style[prop];
-            }
+        applyStyles(color) {
+            this.element.style.position = "fixed";
+            this.element.style.top = "0";
+            this.element.style.display = "block";
+            this.element.style.pointerEvents = "none";
+            this.element.style["z-index"] = "10000000";
+            this.element.style.fontSize = "25px";
+            this.element.style["will-change"] = "transform";
+            this.element.style.color = color;
         }
     }
 
@@ -58,36 +46,37 @@
     }
 
     function render() {
-        for (let i = 0; i < elementGroup.length; i++) {
-            elementGroup[i].update();
-            if (elementGroup[i].lifeSpan < 0) {
-                elementGroup[i].die();
-                elementGroup.splice(i, 1);
+        for (let i = 0; i < ELEMENT_GROUP.length; i++) {
+            ELEMENT_GROUP[i].update();
+            if (ELEMENT_GROUP[i].lifeSpan < 0) {
+                ELEMENT_GROUP[i].die();
+                ELEMENT_GROUP.splice(i, 1);
+                i--; // Adjust index after removal
             }
         }
     }
 
     function onMouseMove(event) {
-        const num = Math.floor(Math.random() * colors.length);
-        createElement(event.clientX, event.clientY, colors[num]);
+        createElement(event.clientX, event.clientY);
     }
 
-    function createElement(x, y, color) {
-        const element = new Element();
-        element.init(x, y, color);
-        elementGroup.push(element);
+    function createElement(x, y) {
+        const color = Array.from(COLORS)[Math.floor(Math.random() * COLORS.size)];
+        const element = new Element(x, y, color);
+        ELEMENT_GROUP.push(element);
     }
 
     function onTouch(event) {
         if (event.touches.length > 0) {
             for (let i = 0; i < event.touches.length; i++) {
-                const num = Math.floor(Math.random() * colors.length);
-                createElement(event.touches[i].clientX, event.touches[i].clientY, colors[num]);
+                createElement(event.touches[i].clientX, event.touches[i].clientY);
             }
         }
     }
 
     addListeners();
-    setInterval(render, 1000 / 60);
+    requestAnimationFrame(function loop() {
+        render();
+        requestAnimationFrame(loop);
+    });
 })();
-
